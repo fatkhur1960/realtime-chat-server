a576d95af17af3ab8bca403aa5280c9727f185c5
fatkhur <fatkhuranonym@gmail.com>
2021-04-18 22:40:22 +0700

    update group typing

--- src/utils/socketHandler.ts
+++ src/utils/socketHandler.ts
@@ -1,47 +1,47 @@
-import { Message, ChatServer, User, PrivateChat } from './chat';
-import Logger from '@ptkdev/logger';
-import socketio from 'socket.io';
+import { Message, ChatServer, User, PrivateChat } from "./chat";
+import Logger from "@ptkdev/logger";
+import socketio from "socket.io";
 
 const allUsers = new Map();
 
-const handleSocket = (io: socketio.Server, chatServer: ChatServer, logger: Logger) => {
+const handleSocket = (
+  io: socketio.Server,
+  chatServer: ChatServer,
+  logger: Logger
+) => {
   io.on("connection", (socket) => {
     var user: User;
 
     logger.info(`New connection - ${socket.id}`);
 
-    socket.on(
-      "register",
-      (regUser: any) => {
+    socket.on("register", (regUser: any) => {
+      user = { ...regUser, idCard: regUser.id_card };
 
-        user = { ...regUser, idCard: regUser.id_card }
+      if (user) {
+        logger.info(`[${user.idCard}] ${user.name} - ${user.role} registered`);
 
-        if (user) {
-          logger.info(`[${user.idCard}] ${user.name} - ${user.role} registered`);
+        allUsers.set(user.idCard, socket.id);
 
-          allUsers.set(user.idCard, socket.id);
-
-          if (user.role === "GURU" || user.role === "BK") {
-            socket.broadcast.emit("teacherOnline", { user, socketId: socket.id });
-            chatServer.addTeacher(user)
-          } else {
-            socket.broadcast.emit("studentOnline", { user, socketId: socket.id });
-            chatServer.addStudent(user)
-          }
+        if (user.role === "GURU" || user.role === "BK") {
+          socket.broadcast.emit("teacherOnline", { user, socketId: socket.id });
+          chatServer.addTeacher(user);
+        } else {
+          socket.broadcast.emit("studentOnline", { user, socketId: socket.id });
+          chatServer.addStudent(user);
         }
       }
-    );
+    });
 
     socket.on("getStats", () => {
       if (user) {
         logger.info(`${user.name} request stats`);
         var onlineUsers = chatServer.onlineStudents;
-        if (user.role === 'SISWA') {
+        if (user.role === "SISWA") {
           onlineUsers = chatServer.onlineTeachers;
         }
-        socket.emit("statsLoaded", { rooms: [], onlineUsers })
+        socket.emit("statsLoaded", { rooms: [], onlineUsers });
       }
-    })
+    });
 
     socket.on("join", (roomId: string) => {
       if (user) {
@@ -59,7 +59,7 @@ const handleSocket = (io: socketio.Server, chatServer: ChatServer, logger: Logge
 
     socket.on(
       "sendMessage",
-      ({ message, roomId }: { message: Message, roomId: string }) => {
+      ({ message, roomId }: { message: Message; roomId: string }) => {
         socket.broadcast.to(roomId).emit("message", message);
         socket.broadcast.emit("roomUpdated", { rooms: [] });
         socket.emit("roomUpdated", { rooms: [] });
@@ -87,37 +87,65 @@ const handleSocket = (io: socketio.Server, chatServer: ChatServer, logger: Logge
     //   }
     // });
 
-    socket.on("getSocketId", ({ idCard, role }: { idCard: string, role: any }) => {
-      if (user) {
-        logger.info(`${user.name} request socket id for ${idCard}`)
+    socket.on(
+      "getSocketId",
+      ({ idCard, role }: { idCard: string; role: any }) => {
+        if (user) {
+          logger.info(`${user.name} request socket id for ${idCard}`);
 
-        const socketId = allUsers.get(idCard);
-        if (socketId) {
-          logger.info(`[${socketId}] socket id for ${idCard}`)
-          socket.emit("gotSocketId", socketId);
+          const socketId = allUsers.get(idCard);
+          if (socketId) {
+            logger.info(`[${socketId}] socket id for ${idCard}`);
+            socket.emit("gotSocketId", socketId);
+          }
         }
       }
-    });
+    );
 
-    socket.on("sendPrivateMessage", ({ socketId, message, opponent, senderId, conversationId }: { socketId: string, message: Message, senderId: any, opponent: User, conversationId: number }) => {
-      // send message to opponent socket id
-      socket.to(socketId).emit("gotPrivateMessage", { message, user: opponent, senderId, conversationId })
-      // update opponent rooms
-      socket.to(socketId).emit("roomUpdated", { rooms: [] })
-      // update current user rooms
-      socket.emit("roomUpdated", { rooms: [] })
-    });
+    socket.on(
+      "sendPrivateMessage",
+      ({
+        socketId,
+        message,
+        opponent,
+        senderId,
+        conversationId,
+        subject,
+      }: {
+        socketId: string;
+        message: Message;
+        senderId: any;
+        opponent: User;
+        conversationId: number;
+        subject: any;
+      }) => {
+        // send message to opponent socket id
+        socket
+          .to(socketId)
+          .emit("gotPrivateMessage", {
+            message,
+            user: opponent,
+            senderId,
+            conversationId,
+            subject,
+          });
+        // update opponent rooms
+        socket.to(socketId).emit("roomUpdated", { rooms: [] });
+        // update current user rooms
+        socket.emit("roomUpdated", { rooms: [] });
+      }
+    );
 
     socket.on("privateTyping", (socketId) => {
       if (user) {
-        socket.to(socketId).emit("typing", { roomId: user.idCard, who: user })
+        socket.to(socketId).emit("typing", { roomId: user.idCard, who: user });
       }
     });
 
-    socket.on("typing", (roomId) => {
+    socket.on("groupTyping", (roomId) => {
       if (user) {
         socket.broadcast.to(roomId).emit("typing", { roomId, who: user });
-        socket.broadcast.emit("bcTyping", { roomId, who: user });
+        socket.broadcast.emit("typing", { roomId, who: user });
       }
     });
 
@@ -129,10 +157,10 @@ const handleSocket = (io: socketio.Server, chatServer: ChatServer, logger: Logge
 
         if (user.role === "GURU" || user.role === "BK") {
           socket.broadcast.emit("teacherOffline", user);
-          chatServer.removeTeacher(user)
+          chatServer.removeTeacher(user);
         } else {
           socket.broadcast.emit("studentOffline", user);
-          chatServer.removeStudent(user)
+          chatServer.removeStudent(user);
         }
       }
     });
